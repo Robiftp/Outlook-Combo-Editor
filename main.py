436a514ad9786766662fdc90627f5bcc19645371
Robiftp <67561687+Robiftp@users.noreply.github.com>
2024-12-26 13:02:17 +0100

    Update main.py
    New domains added

--- main.py
+++ main.py
@@ -2,41 +2,43 @@ import re
 
 def filter_microsoft_emails(input_file, output_file):
     microsoft_domains = [
-        "@hotmail.com", "@live.com", "@outlook.com", "@hotmail.co.uk", "@hotmail.de",
-        "@hotmail.fr", "@hotmail.it", "@hotmail.ca", "@live.co.uk", "@live.de",
-        "@live.fr", "@live.com.au", "@live.jp", "@outlook.co.uk", "@outlook.de",
-        "@outlook.fr", "@outlook.jp", "@outlook.in", "@outlook.com.br", "@msn.com",
-        "@passport.com", "@windowslive.com", "@office365.com", "@email.com",
-        "@live.com.ph", "@outlook.com.ph", "@outlook.office365.com", "@microsoft.com"
+        "@hotmail.com", "@hotmail.es", "@hotmail.be", "@hotmail.ca", "@hotmail.ch",
+        "@hotmail.cl", "@hotmail.co.jp", "@hotmail.co.th", "@hotmail.co.uk",
+        "@hotmail.com.ar", "@hotmail.com.au", "@hotmail.com.br", "@hotmail.com.hk",
+        "@hotmail.com.tr", "@hotmail.de", "@hotmail.dk", "@hotmail.fr", "@hotmail.gr",
+        "@hotmail.hu", "@hotmail.it", "@hotmail.live", "@hotmail.my", "@hotmail.nl",
+        "@hotmail.no", "@hotmail.se", "@hotmail.sg", "@live.com", "@live.be",
+        "@live.ca", "@live.cl", "@live.cn", "@live.co.kr", "@live.co.uk", "@live.com.ar",
+        "@live.com.au", "@live.com.mx", "@live.com.pt", "@live.com.sg", "@live.de",
+        "@live.dk", "@live.fi", "@live.fr", "@live.it", "@live.jp", "@live.nl",
+        "@live.no", "@live.ru", "@live.se", "@msn.com", "@msn.se", "@outlook.com",
+        "@outlook.at", "@outlook.be", "@outlook.cl", "@outlook.co.id", "@outlook.co.th",
+        "@outlook.com.ar", "@outlook.com.au", "@outlook.com.br", "@outlook.com.vn",
+        "@outlook.cz", "@outlook.de", "@outlook.dk", "@outlook.es", "@outlook.fr",
+        "@outlook.hu", "@outlook.it", "@outlook.jp", "@outlook.kr", "@outlook.my",
+        "@outlook.pt", "@outlook.sa", "@windowslive.com"
     ]
     
     microsoft_pattern = re.compile(
-        rf"\b[\w.%+-]+({'|'.join(map(re.escape, microsoft_domains))})\b"
+        rf"\b[\w.%+-]+({'|'.join(map(re.escape, microsoft_domains))})\b",
+        re.IGNORECASE
     )
-    
-    seen_emails = set()
     
     with open(input_file, "r") as infile, open(output_file, "w") as outfile:
         for line in infile:
             line = line.strip()
             if not line:
                 continue
-            
             parts = line.split()
             filtered_parts = []
-            
             for part in parts:
                 match = microsoft_pattern.search(part)
                 if match:
                     filtered_parts.append(part)
                 elif not re.search(r"\b[\w.%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b", part):
                     filtered_parts.append(part)
-            
-            filtered_line = " ".join(filtered_parts)
-            
-            if filtered_line and filtered_line not in seen_emails:
-                seen_emails.add(filtered_line)
-                outfile.write(filtered_line + "\n")
+            if filtered_parts:
+                outfile.write(" ".join(filtered_parts) + "\n")
 
 input_file_path = "input_emails.txt"
 output_file_path = "filtered_emails.txt"
